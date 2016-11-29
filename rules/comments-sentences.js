8d71185a65b4ae23f858eadf39bbba90a972250b
Clément PREVOT <clement@lumapps.com>
2016-11-29 17:35:42 +0100

    fix(comments-sentences): fix the case of empty lines
    Also fix the support of the "ignorePattern" option.
    Also add new tests

--- rules/comments-sentences.js
+++ rules/comments-sentences.js
@@ -18,17 +18,22 @@ const PUNCTUATION = /[!.?]$/;
 const ALWAYS_MESSAGE = 'Comment should be a correct sentence';
 const DEFAULT_IGNORE_PATTERN = /^\s*(?:eslint|istanbul|jscs|jshint|globals?|exported|@.+)\b/;
 const WHITESPACE = /\s/g;
-// TODO: Combine w/ max-len pattern;
 const MAYBE_URL = /^\s*[^:/?#\s]+:\/\/[^?#]/;
 const DEFAULTS = {
     ignorePattern: null,
     ignoreInlineComments: false,
 };
 
+const SPACE_ERROR_MESSAGE = 'Not starting by spaces';
+const UPPERCASE_ERROR_MESSAGE = 'Not starting by an uppercase letter while it should';
+const LOWERCASE_ERROR_MESSAGE = 'Starting by an uppercase letter while it should not';
+const PUNCTUATION_ERROR_MESSAGE = 'Line does not end by a punctuation';
+
 /*
- * Base schema body for defining the basic pattern, ignorePattern,
- * and ignoreInlineComments values.
- * This can be used in a few different ways in the actual schema.
+ * Base schema body for defining the basic pattern, ignorePattern, and ignoreInlineComments values. This can be used in
+ * a few different ways in the actual schema.
+ *
+ * @type {Object}
  */
 const SCHEMA_BODY = {
     type: 'object',
@@ -44,43 +49,22 @@ const SCHEMA_BODY = {
 };
 
 /**
- * Get normalized options for either block or line comments from the given
- * user-provided options.
- * - If the user-provided options is just a string, returns a normalized
- *   set of options using default values for all other options.
- * - If the user-provided options is an object, then a normalized option
- *   set is returned. Options specified in overrides will take priority
- *   over options specified in the main options object, which will in
- *   turn take priority over the rule's defaults.
+ * Get normalized options for either block or line comments from the given user-provided options.
+ *     - If the user-provided options is just a string, returns a normalized set of options using default values for all
+ *       other options.
+ *     - If the user-provided options is an object, then a normalized option set is returned. Options specified in
+ *       overrides will take priority over options specified in the main options object, which will in turn take
+ *       priority over the rule's defaults.
  *
- * @param {Object|string} rawOptions The user-provided options.
- * @param {string} which Either 'line' or 'block'.
- * @returns {Object} The normalized options.
+ * @param  {Object|string} rawOptions The user-provided options.
+ * @return {Object}        The normalized options.
  */
-function getNormalizedOptions(rawOptions, which) {
-    if (!rawOptions) {
+function getNormalizedOptions(rawOptions) {
+    if (rawOptions === undefined) {
         return Object.assign({}, DEFAULTS);
     }
 
     return Object.assign({}, DEFAULTS, rawOptions);
-}
-
-/**
- * Creates a regular expression for each pattern defined in the rule
- * options.
- *
- * This is done in order to avoid invoking the RegExp constructor repeatedly.
- *
- * @param {Object} normalizedOptions The normalized rule options.
- * @returns {void}
- */
-function createRegExpForPatterns(normalizedOptions) {
-    const ignorePatternStr = normalizedOptions.ignorePattern;
-    if (ignorePatternStr) {
-        const regExp = RegExp(`^\\s*(?:${ignorePatternStr})`);
-
-        normalizedOptions.ignorePatternRegExp = regExp;
-    }
 }
 
 //------------------------------------------------------------------------------
@@ -89,7 +73,8 @@ function createRegExpForPatterns(normalizedOptions) {
 // {
 // meta: {
 //     docs: {
-//         description: 'enforce that comments are correct sentences (basically starting with an upper case letter and ending with a dot',
+//         description: 'enforce that comments are correct sentences (basically starting with an upper case letter and
+//                       ending with a dot',
 //         category: 'Stylistic Issues',
 //         recommended: false
 //     },
@@ -101,10 +86,8 @@ function createRegExpForPatterns(normalizedOptions) {
 
 // create
 module.exports = function(context) {
-    const normalizedOptions = getNormalizedOptions(context.options[1]);
+    const normalizedOptions = getNormalizedOptions(context.options[0]);
     const sourceCode = context.getSourceCode();
-
-    createRegExpForPatterns(normalizedOptions);
 
     //----------------------------------------------------------------------
     // Helpers
@@ -114,22 +97,20 @@ module.exports = function(context) {
      * Checks whether a comment is an inline comment.
      *
      * For the purpose of this rule, a comment is inline if:
-     * 1. The comment is preceded by a token on the same line; and
-     * 2. The command is followed by a token on the same line.
+     *     1. The comment is preceded by a token on the same line; and
+     *     2. The command is followed by a token on the same line.
      *
      * Note that the comment itself need not be single-line!
      *
-     * Also, it follows from this definition that only block comments can
-     * be considered as possibly inline. This is because line comments
-     * would consume any following tokens on the same line as the comment.
+     * Also, it follows from this definition that only block comments can be considered as possibly inline. This is
+     * because line comments would consume any following tokens on the same line as the comment.
      *
-     * @param {ASTNode} comment The comment node to check.
-     * @returns {boolean} True if the comment is an inline comment, false
-     * otherwise.
+     * @param  {ASTNode} comment The comment node to check.
+     * @return {boolean} True if the comment is an inline comment, false otherwise.
      */
     function isInlineComment(comment) {
-        const previousToken = sourceCode.getTokenOrCommentBefore(comment),
-            nextToken = sourceCode.getTokenOrCommentAfter(comment);
+        const previousToken = sourceCode.getTokenOrCommentBefore(comment);
+        const nextToken = sourceCode.getTokenOrCommentAfter(comment);
 
         return Boolean(
             previousToken &&
@@ -142,13 +123,18 @@ module.exports = function(context) {
     /**
      * Check a comment to determine if it is valid for this rule.
      *
-     * @param {ASTNode} comment The comment node to process.
-     * @param {Object} options The options for checking this comment.
-     * @returns {boolean} True if the comment is valid, false otherwise.
+     * @param  {ASTNode} comment The comment node to process.
+     * @param  {Object}  options The options for checking this comment.
+     * @return {boolean} True if the comment is valid, false otherwise.
      */
     function isCommentValid(comment, options) {
+        var col = 0;
+        var lineNumber;
+        var message;
+        var more;
+
         // 1. Check for inline comments.
-        if (options.ignoreInlineComments && isInlineComment(comment)) {
+        if (options.ignoreInlineComments === true && isInlineComment(comment)) {
             return true;
         }
 
@@ -156,19 +142,20 @@ module.exports = function(context) {
         var lines = comment.value.split('\n') || [comment.value];
 
         var line;
-        var previousLine;
         var isIgnored = true;
         var wasIgnored = true;
         var isEmpty = true;
         var wasEmpty = true;
         var isEndingByPunctuation = true;
         var wasEndingByPunctuation = true;
-        for (var i = 0, len = lines.length; i < len; i++) {
+
+        var i = 0;
+        var len = lines.length;
+        for (i = 0; i < len; i++) {
             wasIgnored = isIgnored;
             if (!wasIgnored) {
                 wasEmpty = isEmpty;
                 wasEndingByPunctuation = isEndingByPunctuation;
-                previousLine = line;
             }
             isEmpty = false;
 
@@ -183,7 +170,8 @@ module.exports = function(context) {
             }
 
             // 2.2. Check for custom ignore pattern.
-            if (options.ignorePatternRegExp && options.ignorePatternRegExp.test(line)) {
+            if (options.ignorePattern !== undefined && options.ignorePattern !== null &&
+                RegExp(options.ignorePattern).test(line)) {
                 continue;
             }
 
@@ -200,8 +188,12 @@ module.exports = function(context) {
             if (commentWordCharsOnly.length === 0) {
                 isEmpty = true;
 
-                if (!wasEndingByPunctuation) {
-                    return 'Line does not end by a punctuation';
+                if (!wasEmpty && !wasIgnored && !wasEndingByPunctuation) {
+                    message = PUNCTUATION_ERROR_MESSAGE
+                    more = "Next line is empty";
+                    lineNumber = i - 1;
+                    col = line.length + 1;
+                    break;
                 }
 
                 continue;
@@ -210,23 +202,31 @@ module.exports = function(context) {
 
             // 2.5. Check that the comment start with a single space or multiple (at least 2) "/".
             if (!/^(\s+|\/{2,})/.test(line)) {
-                return 'Not starting by spaces';
+                message = SPACE_ERROR_MESSAGE
+                break;
             }
 
             // Remove the useless first space.
             line = line.substring(1);
-            // Is the initial word character a letter?
+            // 2.6. Is the initial word character a letter?
             const firstWordChar = line[0];
             if (FIRST_LETTER_PATTERN.test(firstWordChar)) {
-                // 2.6. Check that if the previous line ended with a punctuation or was ignored, the first letter is
+                // 2.6.1. Check that if the previous line ended with a punctuation or was ignored, the first letter is
                 // uppercase.
-                if ((wasEndingByPunctuation || wasIgnored || wasEmpty) && firstWordChar !== firstWordChar.toLocaleUpperCase()) {
-                    return 'Not starting by an uppercase letter while it should';
+                if ((wasEndingByPunctuation || wasIgnored || wasEmpty) &&
+                    firstWordChar !== firstWordChar.toLocaleUpperCase()) {
+                    message = UPPERCASE_ERROR_MESSAGE;
+                    col = 1;
+                    break;
                 }
-                // 2.6. Check that if the previous line was not ending with a punctuation or was not ignored, the first
-                // letter is lowercase.
-                if (!wasEndingByPunctuation && !wasIgnored && !wasEmpty && firstWordChar !== firstWordChar.toLocaleLowerCase()) {
-                    return 'Starting by an uppercase letter while it should not';
+
+                // 2.6.1. Check that if the previous line was not ending with a punctuation or was not ignored, the
+                // first letter is lowercase.
+                if (!wasEndingByPunctuation && !wasIgnored && !wasEmpty &&
+                    firstWordChar !== firstWordChar.toLocaleLowerCase()) {
+                    message = LOWERCASE_ERROR_MESSAGE;
+                    col = 1;
+                    break;
                 }
             }
 
@@ -234,22 +234,42 @@ module.exports = function(context) {
             isEndingByPunctuation = PUNCTUATION.test(line) || /\/\/$/.test(line);
         }
 
-        if ((isIgnored && !wasEndingByPunctuation) || (!isIgnored && !isEndingByPunctuation)) {
-            return 'Line does not end by a punctuation';
+        if (!isIgnored) {
+            wasEmpty = isEmpty;
+            wasEndingByPunctuation = isEndingByPunctuation;
         }
 
-        return true;
+        if ((message === undefined || message.length === 0) &&
+            ((isEmpty && !wasIgnored && !wasEndingByPunctuation) || (!isEmpty && !isIgnored && !isEndingByPunctuation))) {
+            message = PUNCTUATION_ERROR_MESSAGE;
+            more = "Last line";
+            col = line.length + 1;
+
+            if (i === len) {
+                i--;
+            }
+        }
+
+        lineNumber = (lineNumber === undefined) ? i : lineNumber;
+
+        return (message !== undefined && message.length > 0) ? {
+            message,
+            data: {
+                col: col + 1,
+                commentLine: lines[lineNumber],
+                line: lineNumber + 1,
+                more,
+            },
+        } : true;
     }
 
     /**
      * Process a comment to determine if it needs to be reported.
      *
      * @param {ASTNode} comment The comment node to process.
-     * @returns {void}
      */
     function processComment(comment) {
-        const options = normalizedOptions;
-        const commentValid = isCommentValid(comment, options);
+        const commentValid = isCommentValid(comment, normalizedOptions);
 
         if (commentValid !== true) {
             context.report({
@@ -257,7 +277,8 @@ module.exports = function(context) {
                 node: null,
 
                 loc: comment.loc,
-                message: commentValid,
+                message: commentValid.message,
+                data: commentValid.data,
             });
         }
     }
--- tests/comments-sentences.js
+++ tests/comments-sentences.js
@@ -45,100 +45,113 @@ ruleTester.run('comments-sentences', rule, {
         '/*\n * This is a valid comment,\n * with one long sentence.\n */',
         '////////',
         '/*******/',
-        '// This is a valid comment //',
+        '// This is a valid comment //', {
+            code: '/**\n * Translation Service.\n * Allow to translate a string using pascalprecht angular translate module or using an array of string.\n *\n * You must have angular-translate (pascalprecht.translate) module available to use this service.\n * Remember to declare your language in angular-translate:\n *      $translateProvider.translations("<langCode: en/fr/es, ...>",\n *      {\n *          // Your translations here.\n *      };\n *\n * Remember also to set a default, preferred and fallback language:\n *      // Set the preferred language the same that the browser language.\n *      ($translateProvider.preferredLanguage((navigator.language != null)) ?\n *          navigator.language :\n *          navigator.browserLanguage).split("_")[0].split("-")[0]);\n *\n *      // Default language to display of any other is not available.\n *      $translateProvider.fallbackLanguage("en");\n *\n *\n * You can then use this service to translate:\n *     - an array containing languages: content["en"] = "Test English"; content["fr"] = "Test Français";\n *     - an angular-translate token ("SITE_TITLE");\n */',
+            options: [{
+                ignorePattern: ';\s*$',
+            }, ],
+        }
     ],
 
     invalid: [{
         code: '// this is not a valid comment.',
         errors: [{
             message: UPPERCASE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '// This is not a valid comment',
         errors: [{
             message: PUNCTUATION_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '//This is not a valid comment.',
         errors: [{
             message: SPACE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '//this is not a valid comment',
         errors: [{
             message: SPACE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '// this is not a valid comment',
         errors: [{
             message: UPPERCASE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '// 29 is not a valid comment',
         errors: [{
             message: PUNCTUATION_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '// +42 is not a valid comment',
         errors: [{
             message: PUNCTUATION_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '//_myVar is not a valid comment',
         errors: [{
             message: SPACE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '//"This" is not a valid comment',
         errors: [{
             message: SPACE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '// \'This\' is not a valid comment',
         errors: [{
             message: PUNCTUATION_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '// \'This\' is not a valid comment,\n// that spans on multi-line.',
         errors: [{
             message: PUNCTUATION_ERROR_MESSAGE,
         }, {
             message: UPPERCASE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '/* 42 is not a valid comment,\n * That spans on multi-line.\n */',
         errors: [{
             message: LOWERCASE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '/* _myVar is not a valid comment.\n * that spans on multi-line.\n */',
         errors: [{
             message: UPPERCASE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '/* This is not a valid comment.\n * It spans on multi-line\n */',
         errors: [{
             message: PUNCTUATION_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '/* This is not a valid comment,\n * it spans on multi-line\n */',
         errors: [{
             message: PUNCTUATION_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '/* This is not a valid comment,\n *it spans on multi-line.\n */',
         errors: [{
             message: SPACE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
-        code: '/* This is not a valid comment.\n *Tt spans on multi-line.\n */',
+        code: '/* This is not a valid comment.\n *It spans on multi-line.\n */',
         errors: [{
             message: SPACE_ERROR_MESSAGE,
-        }],
+        }, ],
     }, {
         code: '/**\n * Return the placeholder for a translatable object.\n * If the translatable content has no translation, display a fixed placeholder.\n * Else display the value of the translatable content in the "checkLanguage".\n * Else, display the first available translation\n *\n * @param  {string|Object} content            The translatable content.\n * @param  {string|Object} defaultPlaceholder The placeholder to use if the translatable content has no\n *                                            translations.\n * @param  {string}        [lang=current]     The lang to use.\n * @return {string}        The placeholder.\n */',
         errors: [{
             message: PUNCTUATION_ERROR_MESSAGE,
-        }],
+        }, ],
+    }, {
+        code: '/**\n * Translation Service.\n * Allow to translate a string using pascalprecht angular translate module or using an array of string\n *\n * You must have angular-translate (pascalprecht.translate) module available to use this service.\n * Remember to declare your language in angular-translate:\n *      $translateProvider.translations("<langCode: en/fr/es, ...>",\n *      {\n *          // Your translations here.\n *      };\n *\n * Remember also to set a default, preferred and fallback language:\n *      // Set the preferred language the same that the browser language.\n *      ($translateProvider.preferredLanguage((navigator.language != null)) ?\n *          navigator.language :\n *          navigator.browserLanguage).split("_")[0].split("-")[0]);\n *\n *      // Default language to display of any other is not available.\n *      $translateProvider.fallbackLanguage("en");\n *\n *\n * You can then use this service to translate:\n *     - an array containing languages: content["en"] = "Test English"; content["fr"] = "Test Français";\n *     - an angular-translate token ("SITE_TITLE");\n */',
+        options: [{
+            ignorePattern: ';\s*$',
+        }, ],
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }, ]
     }],
 });
