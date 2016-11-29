c06a497b85dfe89720e6dc485fa1430397bd2a2c
Clément PREVOT <clement@lumapps.com>
2016-11-29 15:05:46 +0100

    chore(NPM): initialize the rules package

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview Lumapps customized ESLint rules.
+ * @author Clément P.
+ */
+'use strict';
+
+module.exports = {
+    rules: {
+        'comments-pattern': require('./rules/comments-pattern')
+    }
+};
