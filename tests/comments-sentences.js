4fef0720a6f6c6897dc54b7d003c4175d787d2ed
Clément PREVOT <clement@lumapps.com>
2016-11-30 17:20:44 +0100

    feat(comments-sentences): improve the variable recognition
    Also lint the file according to eslint rules

--- tests/comments-sentences.js
+++ tests/comments-sentences.js
@@ -9,7 +9,7 @@
 //------------------------------------------------------------------------------
 
 const rule = require('../rules/comments-sentences');
-const RuleTester = require("eslint").RuleTester;
+const RuleTester = require('eslint').RuleTester;
 
 
 //------------------------------------------------------------------------------
@@ -27,9 +27,124 @@ const PUNCTUATION_ERROR_MESSAGE = 'Line does not end by a punctuation';
 
 const ruleTester = new RuleTester();
 ruleTester.run('comments-sentences', rule, {
+    invalid: [{
+        code: '// this is not a valid comment.',
+        errors: [{
+            message: UPPERCASE_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '// This is not a valid comment',
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '//This is not a valid comment.',
+        errors: [{
+            message: SPACE_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '//this is not a valid comment',
+        errors: [{
+            message: SPACE_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '// this is not a valid comment',
+        errors: [{
+            message: UPPERCASE_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '// 29 is not a valid comment',
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '// +42 is not a valid comment',
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '//_myVar is not a valid comment',
+        errors: [{
+            message: SPACE_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '//"This" is not a valid comment',
+        errors: [{
+            message: SPACE_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '// \'This\' is not a valid comment',
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '// \'This\' is not a valid comment,\n// that spans on multi-line.',
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }, {
+            message: UPPERCASE_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '/* 42 is not a valid comment,\n * That spans on multi-line.\n */',
+        errors: [{
+            message: LOWERCASE_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '/* _myVar is not a valid comment.\n * that spans on multi-line.\n */',
+        errors: [{
+            message: UPPERCASE_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '/* This is not a valid comment.\n * It spans on multi-line\n */',
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '/* This is not a valid comment,\n * it spans on multi-line\n */',
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '/* This is not a valid comment,\n *it spans on multi-line.\n */',
+        errors: [{
+            message: SPACE_ERROR_MESSAGE,
+        }],
+    }, {
+        code: '/* This is not a valid comment.\n *It spans on multi-line.\n */',
+        errors: [{
+            message: SPACE_ERROR_MESSAGE,
+        }],
+    }, {
+        // eslint-disable-next-line max-len
+        code: '/**\n * Return the placeholder for a translatable object.\n * If the translatable content has no translation, display a fixed placeholder.\n * Else display the value of the translatable content in the "checkLanguage".\n * Else, display the first available translation\n *\n * @param  {string|Object} content            The translatable content.\n * @param  {string|Object} defaultPlaceholder The placeholder to use if the translatable content has no\n *                                            translations.\n * @param  {string}        [lang=current]     The lang to use.\n * @return {string}        The placeholder.\n */',
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }],
+    }, {
+        // eslint-disable-next-line max-len
+        code: '/**\n * Translation Service.\n * Allow to translate a string using pascalprecht angular translate module or using an array of string\n *\n * You must have angular-translate (pascalprecht.translate) module available to use this service.\n * Remember to declare your language in angular-translate:\n *      $translateProvider.translations("<langCode: en/fr/es, ...>",\n *      {\n *          // Your translations here.\n *      };\n *\n * Remember also to set a default, preferred and fallback language:\n *      // Set the preferred language the same that the browser language.\n *      ($translateProvider.preferredLanguage((navigator.language != null)) ?\n *          navigator.language :\n *          navigator.browserLanguage).split("_")[0].split("-")[0]);\n *\n *      // Default language to display of any other is not available.\n *      $translateProvider.fallbackLanguage("en");\n *\n *\n * You can then use this service to translate:\n *     - an array containing languages: content["en"] = "Test English"; content["fr"] = "Test Français";\n *     - an angular-translate token ("SITE_TITLE");\n */',
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }],
+        options: [{
+            // eslint-disable-next-line no-useless-escape
+            ignorePattern: ';\s*$',
+        }],
+    }, {
+        code: 'var a = 1;\n// a is a variable\nvar b = 2;',
+        errors: [{
+            message: PUNCTUATION_ERROR_MESSAGE,
+        }],
+    }, {
+        code: 'var a = 1;\n// c is not a variable.\nvar b = 2;',
+        errors: [{
+            message: UPPERCASE_ERROR_MESSAGE,
+        }],
+    }],
+
     valid: [
-        'var a = 1;\n// This is a test.\nvar b = 2;',
-        'var a = 1;\n// a is a variable.\nvar b = 2;',
+        // eslint-disable-next-line max-len
+        'function toto() {\n    var a = 1;\n    // a is a variable.\n    // d is a variable too.\n    if (a === 1) { var c = 3 }\n    var f = function f() { var d = 4; }\n    var b = 2;\n}',
         'var a = 1;\n// b is a variable.\nvar b = 2;',
         '// This is a valid comment.',
         '// 29 is a valid comment.',
@@ -49,122 +164,12 @@ ruleTester.run('comments-sentences', rule, {
         '////////',
         '/*******/',
         '// This is a valid comment //', {
+            // eslint-disable-next-line max-len
             code: '/**\n * Translation Service.\n * Allow to translate a string using pascalprecht angular translate module or using an array of string.\n *\n * You must have angular-translate (pascalprecht.translate) module available to use this service.\n * Remember to declare your language in angular-translate:\n *      $translateProvider.translations("<langCode: en/fr/es, ...>",\n *      {\n *          // Your translations here.\n *      };\n *\n * Remember also to set a default, preferred and fallback language:\n *      // Set the preferred language the same that the browser language.\n *      ($translateProvider.preferredLanguage((navigator.language != null)) ?\n *          navigator.language :\n *          navigator.browserLanguage).split("_")[0].split("-")[0]);\n *\n *      // Default language to display of any other is not available.\n *      $translateProvider.fallbackLanguage("en");\n *\n *\n * You can then use this service to translate:\n *     - an array containing languages: content["en"] = "Test English"; content["fr"] = "Test Français";\n *     - an angular-translate token ("SITE_TITLE");\n */',
             options: [{
+                // eslint-disable-next-line no-useless-escape
                 ignorePattern: ';\s*$',
-            }, ],
-        }
+            }],
+        },
     ],
-
-    invalid: [{
-        code: '// this is not a valid comment.',
-        errors: [{
-            message: UPPERCASE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '// This is not a valid comment',
-        errors: [{
-            message: PUNCTUATION_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '//This is not a valid comment.',
-        errors: [{
-            message: SPACE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '//this is not a valid comment',
-        errors: [{
-            message: SPACE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '// this is not a valid comment',
-        errors: [{
-            message: UPPERCASE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '// 29 is not a valid comment',
-        errors: [{
-            message: PUNCTUATION_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '// +42 is not a valid comment',
-        errors: [{
-            message: PUNCTUATION_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '//_myVar is not a valid comment',
-        errors: [{
-            message: SPACE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '//"This" is not a valid comment',
-        errors: [{
-            message: SPACE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '// \'This\' is not a valid comment',
-        errors: [{
-            message: PUNCTUATION_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '// \'This\' is not a valid comment,\n// that spans on multi-line.',
-        errors: [{
-            message: PUNCTUATION_ERROR_MESSAGE,
-        }, {
-            message: UPPERCASE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '/* 42 is not a valid comment,\n * That spans on multi-line.\n */',
-        errors: [{
-            message: LOWERCASE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '/* _myVar is not a valid comment.\n * that spans on multi-line.\n */',
-        errors: [{
-            message: UPPERCASE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '/* This is not a valid comment.\n * It spans on multi-line\n */',
-        errors: [{
-            message: PUNCTUATION_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '/* This is not a valid comment,\n * it spans on multi-line\n */',
-        errors: [{
-            message: PUNCTUATION_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '/* This is not a valid comment,\n *it spans on multi-line.\n */',
-        errors: [{
-            message: SPACE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '/* This is not a valid comment.\n *It spans on multi-line.\n */',
-        errors: [{
-            message: SPACE_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '/**\n * Return the placeholder for a translatable object.\n * If the translatable content has no translation, display a fixed placeholder.\n * Else display the value of the translatable content in the "checkLanguage".\n * Else, display the first available translation\n *\n * @param  {string|Object} content            The translatable content.\n * @param  {string|Object} defaultPlaceholder The placeholder to use if the translatable content has no\n *                                            translations.\n * @param  {string}        [lang=current]     The lang to use.\n * @return {string}        The placeholder.\n */',
-        errors: [{
-            message: PUNCTUATION_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: '/**\n * Translation Service.\n * Allow to translate a string using pascalprecht angular translate module or using an array of string\n *\n * You must have angular-translate (pascalprecht.translate) module available to use this service.\n * Remember to declare your language in angular-translate:\n *      $translateProvider.translations("<langCode: en/fr/es, ...>",\n *      {\n *          // Your translations here.\n *      };\n *\n * Remember also to set a default, preferred and fallback language:\n *      // Set the preferred language the same that the browser language.\n *      ($translateProvider.preferredLanguage((navigator.language != null)) ?\n *          navigator.language :\n *          navigator.browserLanguage).split("_")[0].split("-")[0]);\n *\n *      // Default language to display of any other is not available.\n *      $translateProvider.fallbackLanguage("en");\n *\n *\n * You can then use this service to translate:\n *     - an array containing languages: content["en"] = "Test English"; content["fr"] = "Test Français";\n *     - an angular-translate token ("SITE_TITLE");\n */',
-        options: [{
-            ignorePattern: ';\s*$',
-        }, ],
-        errors: [{
-            message: PUNCTUATION_ERROR_MESSAGE,
-        }, ]
-    }, {
-        code: 'var a = 1;\n// a is a variable\nvar b = 2;',
-        errors: [{
-            message: PUNCTUATION_ERROR_MESSAGE,
-        }, ],
-    }, {
-        code: 'var a = 1;\n// c is not a variable.\nvar b = 2;',
-        errors: [{
-            message: UPPERCASE_ERROR_MESSAGE,
-        }, ],
-    }, ],
 });
